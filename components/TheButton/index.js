82f9bb209e2d19b9dbdf64d8ffb600f891ab09d3
entrptaher@gmail.com <entrptaher@gmail.com>
2019-10-05 06:03:07 +0600

    add global mixin

--- components/TheButton/index.js
+++ components/TheButton/index.js
@@ -4,7 +4,8 @@ import styles from './style.less';
 
 const TheButton = () => (
   <div className={styles.Container}>
-    <Button className={styles.BigBtn}>Test</Button>
+    <Button>Small Button</Button>
+    <Button className={styles.BigBtn}>Big Button</Button>
   </div>
 )
 
